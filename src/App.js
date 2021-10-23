d7d0bf25f721aced3caaf46cf01351d28fecf5c0
AnthonyFelefly <anthony.felefly@net.usj.edu.lb>
2021-10-23 18:46:05 +0300

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import {  Navbar, NavbarBrand } from 'reactstrap';
 import { Component } from 'react';
+import Catalogue from './components/CatalogueComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
         <Navbar className="navbar" dark>
           <div className="container">
             <NavbarBrand href="/">Pharmacy Absolute Care</NavbarBrand>
           </div>
         </Navbar>
+        <Catalogue />
       </div>
     );
   }
