e99fe5e33da8d4fe539df04079e87de9e3d91ae9
Tony <tony_dfouny@hotmail.com>
2021-11-15 01:58:42 +0200

    confirm password

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -6,7 +6,6 @@ function Home(){
     return(
         <div>
         <MDBContainer>
-<<<<<<< HEAD
         <MDBCarousel
         activeItem={1}
         length={3}
@@ -71,20 +70,6 @@ function Home(){
 
             <MDBCol size="2">
               <MDBIcon icon="shopping-cart" size="3x" />
-=======
-        <MDBRow>
-            <MDBCol>
-            <MDBJumbotron style={{ padding: 0,width:"1024px",height:"700px"}}>
-                <MDBCol className="text-black text-center py-5 px-4 my-5 " style={{backgroundImage: 'url(assets/images/JumbotronBg.png)',height:"700px",width:"1024px",backgroundSize:"100% 100%",backgroundRepeat:"no-repeat"}}>
-                <MDBCol className="py-5">
-                {/* <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Create your beautiful website with MDBootstrap</MDBCardTitle>
-                    <p className="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                    optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
-                    </p> */}
-                   </MDBCol>
-                </MDBCol>
-            </MDBJumbotron>
->>>>>>> 0bceb36c1b882c90fa9241164513a8a875917a6c
             
               <h5 className="font-weight-bold mb-3">Wide variety of products</h5>
             </MDBCol>
