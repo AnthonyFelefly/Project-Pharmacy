8ad4fa9f7f09184df593252267df54f69d0988d9
AnthonyFelefly <anthony.felefly@net.usj.edu.lb>
2021-11-29 04:28:53 +0200

    Admin Add_Product Action fixed

--- src/redux/products.js
+++ src/redux/products.js
@@ -10,9 +10,9 @@ export const Products=(state={
     switch(action.type){
         case ActionTypes.ADD_PRODUCT:
             var product=action.payload;
-            product.id=state.length;
+            product.id=state.products.length;
             console.log(product);
-            return state.concat(product)
+            return {...state,isLoading:false,errMess:null,products:state.products.concat(product)}
         case ActionTypes.ADD_PRODUCTS:
             return {...state,isLoading:false,errMess:null,products:action.payload};
 
