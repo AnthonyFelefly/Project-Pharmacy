9f3a66fb0ec75b1e2993a7758537e424338c80aa
AnthonyFelefly <anthony.felefly@net.usj.edu.lb>
2022-01-09 18:09:13 +0200

    cart functions fixed

--- src/redux/products.js
+++ src/redux/products.js
@@ -23,21 +23,21 @@ export const Products=(state={
             return {...state,isLoading:false,errMess:action.payload,products:[]};
         case ActionTypes.ADD_TO_CART:
             console.log("add to cart applied");
-            const item=state.products.find(prod=>prod.id===action.payload.productId);
+            const item=state.products.find(prod=>prod._id===action.payload.productId);
             console.log("aaaa");
-            const inCart=state.cart.find((item)=>item.id===action.payload.productId)?true:false;
+            const inCart=state.cart.find((item)=>item._id===action.payload.productId)?true:false;
             
             return {...state,cart:inCart?
-                state.cart.map((item)=>item.id===action.payload.productId?{...item,quantity:item.quantity+1}:item)
+                state.cart.map((item)=>item._id===action.payload.productId?{...item,quantity:item.quantity+1}:item)
                 :[...state.cart,{...item,quantity:1}],
             }
         case ActionTypes.REMOVE_FROM_CART:
             return {...state,
-            cart:state.cart.filter(item=>item.id!==action.payload.productId)};
+            cart:state.cart.filter(item=>item._id!==action.payload.productId)};
 
         case ActionTypes.ADJUST_QTY_CART:
             return {...state,
-            cart:state.cart.map(item=>item.id===action.payload.productId?{...item,quantity:+action.payload.quantity}:item)};
+            cart:state.cart.map(item=>item._id===action.payload.productId?{...item,quantity:+action.payload.quantity}:item)};
 
         case ActionTypes.LOAD_CURRENT_ITEM:
             return {...state,currentProduct:action.payload};
